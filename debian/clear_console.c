b658a5057a2bc61cd8402d2e114c8504a08bd2b0
Matthias Klose <doko@debian.org>
2006-03-23 01:16:22 +0100

    Import Debian changes 3.1-4
    bash (3.1-4) unstable; urgency=low
    
      * Update upstream patch 010, apply upstream patches 012, 013, 014.
      * bash-default-editor.dpatch: Fix typo (closes: #356308).
      * clear_console: Add FreeBSD support (Aurelian Jarno). Closes: #355336.
      * clear_console: Only get rid of any history in the scrollback, if
        not running in a pseudo tty (closes: #355815).
      * bash_completion:
        - Fix error in minicom completion (closes: #357241).

--- debian/clear_console.c
+++ debian/clear_console.c
@@ -4,9 +4,15 @@
 #include <string.h>
 #include <getopt.h>
 #include <errno.h>
-#include <linux/kd.h>
+#include <unistd.h>
 #include <sys/ioctl.h>
+#if defined(__linux)
+#include <linux/kd.h>
 #include <linux/vt.h>
+#elif defined(__FreeBSD_kernel__)
+#include <sys/consio.h>
+#include <sys/kbio.h>
+#endif
 
 #include <curses.h>
 #include <term.h>
@@ -14,6 +20,7 @@
 #define VERSION "0.1"
 
 char* progname;
+int quiet = 0;
 
 void usage()
 {
@@ -43,11 +50,15 @@ static int putch(int c)
 
 int is_a_console(int fd)
 {
+#if defined(__linux__)
   char arg;
+#elif defined(__FreeBSD_kernel__)
+  int arg;
+#endif
 
   arg = 0;
   return (ioctl(fd, KDGKBTYPE, &arg) == 0
-          && ((arg == KB_101) || (arg == KB_84)));
+          && ((arg == KB_OTHER) || (arg == KB_101) || (arg == KB_84)));
 }
 
 static int open_a_console(char *fnam)
@@ -124,13 +135,91 @@ int get_console_fd(char* tty_name)
 }
 
 
+int is_pseudo_tty(int fd)
+{
+  char *tty = ttyname(fd);
+
+  if (!strncmp(tty, "/dev/pts/", 9))
+    return 1;
+
+  if (!strncmp(tty, "/dev/tty", 8) && tty[8] >= 'a' && tty[8] <= 'z')
+    return 1;
+
+  return 0;
+}
+
+int clear_console(int fd)
+{
+  int num, tmp_num;
+#if defined(__linux__)
+  struct vt_stat vtstat;
+#endif
+
+  /* clear screen */
+  setupterm((char *) 0, 1, (int *) 0);
+  if (tputs(clear_screen, lines > 0 ? lines : 1, putch) == ERR)
+    {
+      exit(1);
+    }
+
+  if (is_pseudo_tty(STDIN_FILENO))
+    return 0;
+
+  if (!strcmp(getenv("TERM"), "screen"))
+      return 0;
+
+  /* get current vt */
+#if defined(__linux__)
+  if (ioctl(fd, VT_GETSTATE, &vtstat) < 0)
+#elif defined(__FreeBSD_kernel__)
+  if (ioctl(fd, VT_ACTIVATE, &num) < 0)
+#endif
+    {
+      if (!quiet)
+	fprintf(stderr, "%s: cannot get VTstate\n", progname);
+      exit(1);
+    }
+#if defined(__linux__)
+  num = vtstat.v_active;
+#endif
+  tmp_num = (num == 1 ? 2 : 1);
+
+  /* switch vt to clear the scrollback buffer */
+  if (ioctl(fd, VT_ACTIVATE, tmp_num))
+    {
+      if (!quiet)
+	perror("chvt: VT_ACTIVATE");
+      exit(1);
+    }
+
+  if (ioctl(fd, VT_WAITACTIVE, tmp_num))
+    {
+      if (!quiet)
+	perror("VT_WAITACTIVE");
+      exit(1);
+    }
+
+  /* switch back */
+  if (ioctl(fd, VT_ACTIVATE, num))
+    {
+      if (!quiet)
+	perror("chvt: VT_ACTIVATE");
+      exit(1);
+    }
+
+  if (ioctl(fd, VT_WAITACTIVE, num))
+    {
+      if (!quiet)
+	perror("VT_WAITACTIVE");
+      exit(1);
+    }
+}
+
 int main (int argc, char* argv[])
 {
-  int fd, num, tmp_num;
+  int fd;
   int result;                          /* option handling */
   int an_option;
-  int quiet = 0;
-  struct vt_stat vtstat;
 
   if ((progname = strrchr(argv[0], '/')) == NULL)
     progname = argv[0];
@@ -165,13 +254,6 @@ int main (int argc, char* argv[])
       exit(1);
     }
 
-  if (getenv("DISPLAY"))
-    {
-      if (!quiet)
-	fprintf(stderr, "%s: not a console (DISPLAY is set)\n", progname);
-      exit(1);
-    }
-
   if ((fd = get_console_fd(NULL)) == -1)
     {
       if (!quiet)
@@ -179,54 +261,7 @@ int main (int argc, char* argv[])
       exit(1);
     }
 
-  /* clear screen */
-  setupterm((char *) 0, 1, (int *) 0);
-  if (tputs(clear_screen, lines > 0 ? lines : 1, putch) == ERR)
-    {
-      exit(1);
-    }
-
-  /* get current vt */
-  if (ioctl(fd, VT_GETSTATE, &vtstat) < 0)
-    {
-      if (!quiet)
-	fprintf(stderr, "%s: cannot get VTstate\n", progname);
-      exit(1);
-    }
-  num = vtstat.v_active;
-  tmp_num = (num == 1 ? 2 : 1);
-
-  /* switch vt to clear the scrollback buffer */
-  if (ioctl(fd, VT_ACTIVATE, tmp_num))
-    {
-      if (!quiet)
-	perror("chvt: VT_ACTIVATE");
-      exit(1);
-    }
-
-  if (ioctl(fd, VT_WAITACTIVE, tmp_num))
-    {
-      if (!quiet)
-	perror("VT_WAITACTIVE");
-      exit(1);
-    }
-
-  /* switch back */
-  if (ioctl(fd, VT_ACTIVATE, num))
-    {
-      if (!quiet)
-	perror("chvt: VT_ACTIVATE");
-      exit(1);
-    }
-
-  if (ioctl(fd, VT_WAITACTIVE, num))
-    {
-      if (!quiet)
-	perror("VT_WAITACTIVE");
-      exit(1);
-    }
+  clear_console(fd);
 
   return 0;
 }
-
-
